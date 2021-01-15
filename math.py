15b838be94ca29984bbc5e43c0450bd3ee0d1538
Rumit m Gevariya <77478366+RumitGevariya@users.noreply.github.com>
2021-01-15 16:29:59 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,14 @@
+#python module
+#1)math module:
+#exmaple
+import math
+print(math.factorial(5))
+l1 =[1,2,3,4,5,9]
+print(max(l1))
+print(min(l1))
+print(math.sqrt(2))
+print(pow(4,3))
+print(4**2)
+print(15//2)
+
+
